7e814abe08acffdbcce1698f955ebdf5e7923eef
Quan Chau <quanqchau@gmail.com>
2016-02-01 21:35:20 -0800

    first try of master loop, no test yet

---
+++ master_loop.py
@@ -0,0 +1,111 @@
+import sqsclient
+import time
+import re
+import commonlib
+
+datafile = None
+
+def get_data_file_to_work(message):
+    return message.rstrip().split(',')[1]
+
+def wait_job_start_loop():
+    inloop = True
+    message = None
+    while inloop:
+        message = sqs_client.receive_message('start_job')
+        if message is None:
+            time.sleep(2)
+        else:
+            inloop = False
+
+    global datafile
+    datafile = get_data_file_to_work(message)
+    if re.search('reference_hash', message):
+        start_create_hash_from_ref()
+    elif re.search('align_base', message):
+        start_align_base()
+    elif re.search('get_mutation', message):
+        start_get_mutation()
+
+def start_create_hash_from_ref():
+    global datafile
+
+    print "start creating hash from reference"
+
+    # delete all saved reference genome hash
+    db = database.create_database_connection()
+    db.execute("DELETE FROM reference_hash")
+
+    reference_arr = commonlib.read_reference_genome('dataset/%d/ref.txt' % datafile)
+    offset = 1000
+    for i in xrange(0, len(reference_arr), offset):
+        sqsclient.send_message('reference_hash', '%s,%d,%d' % (datafile, i, i+offset))
+
+    wait_reference_hash_loop()
+
+def wait_reference_hash_loop():
+    inloop = True
+    while inloop:
+        remaning_reference_hash = sqs_client.get_queue_remaining_messages('reference_hash')
+        if remaning_reference_hash > 0:
+            time.sleep(5)
+        else:
+            inloop = False
+    start_align_base()
+
+def start_align_base():
+    global datafile
+
+    print "start aligning bases"
+
+    # delete all saved reference genome hash
+    db = database.create_database_connection()
+    db.execute("DELETE FROM reference_hash")
+
+    reads = commonlib.read_all_reads("dataset/%s/reads.txt" % datafile)
+    offset = 1
+    for i in xrange(0, len(reference_arr), offset):
+        sqsclient.send_message('align_base', '%s,%d,%d' % (datafile, i, i+offset))
+
+    wait_align_base_loop()
+
+def wait_align_base_loop():
+    inloop = True
+    while inloop:
+        remaining_align_base = sqs_client.get_queue_remaining_messages('align_base')
+        if remaining_align_base > 0:
+            time.sleep(5)
+        else:
+            inloop = False
+    start_get_mutation()
+
+def start_get_mutation():
+
+    global datafile
+
+    print "start getting mutations"
+
+    # delete all saved reference genome hash
+    db = database.create_database_connection()
+    db.execute("DELETE FROM mutation")
+
+    reference_arr = commonlib.read_reference_genome('dataset/%d/ref.txt' % datafile)
+    offset = 5
+    for i in xrange(0, len(reference_arr), offset):
+        sqsclient.send_message('get_mutation', '%s,%d,%d' % (datafile, i, i+offset))
+
+    wait_get_mutation_loop()
+
+def wait_get_mutation_loop():
+    inloop = True
+    while inloop:
+        remaining_ref_idx = sqs_client.get_queue_remaining_messages('get_mutation')
+        if remaining_ref_idx > 0:
+            time.sleep(5)
+        else:
+            inloop = False
+    wait_job_start_loop()
+
+if __name__ == "__main__":
+    # wait for start job signal
+    wait_job_start_loop()
--- sqs_client.py
+++ sqs_client.py
@@ -1,3 +1,4 @@
+import ipdb
 import boto3
 import os
 
@@ -6,7 +7,8 @@ sqs = boto3.resource('sqs')
 all_queues = {
     "reference_hash" : sqs.get_queue_by_name(QueueName='cm124_reference_hash_queue'),
     "align_base": sqs.get_queue_by_name(QueueName='cm124_align_base_queue'),
-    "get_mutation": sqs.get_queue_by_name(QueueName='cm124_get_mutation_queue')
+    "get_mutation": sqs.get_queue_by_name(QueueName='cm124_get_mutation_queue'),
+    "start_job": sqs.get_queue_by_name(QueueName='cm124_start_job_queue'),
 }
 
 def send_message(queue_name, message):
@@ -15,11 +17,21 @@ def send_message(queue_name, message):
 
 def receive_message(queue_name):
     global all_queues
-    return all_queues[queue_name].receive_messages(MaxNumberOfMessages=1)[0]
+    messages = all_queues[queue_name].receive_messages(MaxNumberOfMessages=1)
+    if len(messages) > 0:
+        return messages[0]
+    return None
 
+def get_queue_remaining_messages(queue_name):
+    global all_queues
+    all_queues[queue_name].load()
+    # ipdb.set_trace()
+    return int(all_queues[queue_name].attributes['ApproximateNumberOfMessages']) + int(all_queues[queue_name].attributes['ApproximateNumberOfMessagesNotVisible'])
 
 if __name__ == "__main__":
-    send_message('reference_hash', '0,1000')
+    # send_message('reference_hash', '0,1000')
     new_message=receive_message('reference_hash')
-    print new_message.body
-    new_message.delete()
+    if new_message is not None:
+        print new_message.body
+        print get_queue_remaining_messages('reference_hash')
+        new_message.delete()
