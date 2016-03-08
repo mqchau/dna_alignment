9df7e42805516dcb8cce1a445dd66dd90d34c69c
Quan Chau <quanqchau@gmail.com>
2016-03-07 20:45:54 -0800

    more work on clustering match locations

--- align_single_read.py
+++ align_single_read.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 import ipdb
 
 # class location_cluster:
@@ -18,37 +19,47 @@ import ipdb
 #         for one_loc in self.location:
 
         
-# class location:
-#     def __init__(self, location, expected_loc_idx):
-#         self.location = location
-#         self.expected_loc_idx = expected_loc_idx
+class location:
+    def __init__(self, location, expected_loc_idx):
+        self.location = location
+        self.expected_loc_idx = expected_loc_idx
 
-def in_range(cluster, location):
+    def __repr__(self):
+        return str(self.location)
+
+def get_cluster_expected_start(cluster):
+    # print cluster
+    expected_start = map(lambda x: x.location - x.expected_loc_idx, list(cluster))
+    return np.round(np.mean(expected_start))
+
+
+def in_range(cluster, new_loc):
     # if location > min(cluster) and location < max(cluster):
     #     return True
-    if location > min(cluster) - 25 and location < max(cluster) + 25:
-        # ipdb.set_trace()
+    cluster_expected_start = get_cluster_expected_start(cluster)
+    if new_loc.location - new_loc.expected_loc_idx > cluster_expected_start - 10 and new_loc.location - new_loc.expected_loc_idx < cluster_expected_start + 10:
         return True
 
     return False
 
 def clusterize_matches(matches):
     cluster = []
-    for one_sub_read_matches in matches:
+    for sub_read_idx, one_sub_read_matches in enumerate(matches):
         if len(cluster) == 0:
-            cluster.extend(map(lambda x: set([x]), one_sub_read_matches))
+            cluster.extend(map(lambda x: [location(x, sub_read_idx*10)], one_sub_read_matches))
         else:
-            for one_match in one_sub_read_matches: 
+            for  one_match in one_sub_read_matches: 
                 not_in_range_anywhere = True
+                new_loc = location(one_match, sub_read_idx * 10)
                 for one_cluster in cluster:
-                    if in_range(one_cluster, one_match):
+                    if in_range(one_cluster, new_loc):
                         not_in_range_anywhere = False
-                        one_cluster.add(one_match)
+                        one_cluster.append(new_loc)
                         break
 
                 if not_in_range_anywhere:
                     # print "not_in_range_anywhere %d" % one_match
-                    cluster.append(set([one_match]))
+                    cluster.append([new_loc])
 
     return cluster
 
