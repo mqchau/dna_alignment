07ea49a55c7614c58e46eb6a29d7a27f972caae9
Quan Chau <quanqchau@gmail.com>
2016-04-19 21:56:28 -0700

    new reference hash code to redis

--- reference_hash.py
+++ reference_hash.py
@@ -1,77 +1,31 @@
 import commonlib
-# import database
-import pickle
+import redis
+import os
 
-all_ref = {}
+redis_path = os.environ["REDIS_HOST"] if "REDIS_HOST" in os.environ else "localhost"
 mer_length = 10
+dataset = "10k" # 10k, 1m or 100m
 
-def save_hash_location(mer_str, location):
-    db = database.create_database_connection()
-    # try to see if this mer already exists
+def create_reference_hash_save_redis(mer_length, reference, redis_path, redis_db=0):
+    
+    # create redis client
+    r = redis.StrictRedis(host=redis_path, port=6379, db=redis_db)
+    # delete all keys, all hashed locations
+    r.flushall()
 
-    query = db.execute("SELECT location FROM reference_hash WHERE mer = '%s'" % mer_str)
-    matches = query.fetchone()
-    if matches is None:
-        db.execute("INSERT INTO reference_hash (mer, location) VALUES ('%s', '{%d}')" % (mer_str, location))
-    else:
-        # if this is not working then we have duplicate mer, so we update thelist of location
-        db.execute("UPDATE reference_hash SET location = array_append(location, %d) WHERE mer = '%s'" % (location, mer_str))
-
-
-def create_reference_hash(start_idx, stop_idx, mer_length, reference):
-
-    # when stop idx is too large
-    if stop_idx + mer_length >= len(reference):
-        stop_idx = len(reference) - mer_length
-
-    for i in xrange(start_idx, stop_idx):
-        if i % 10000 == 0:
-            print "finished %d/%d, percent=%.2f" % (i, stop_idx - start_idx, (i * 100 / (stop_idx-start_idx)))
+    # iterate through all the length of the reference
+    for i in range(len(reference) - mer_length):
+        if i % 100000 == 0:
+            # some printf for progress
+            print("finished %d/%d, percent=%.2f" % (i, len(reference), (i * 100 / len(reference))))
+        # get the mer string
         mer = reference[i:i+mer_length]
-        mer_str = commonlib.get_string_from_mer(mer)
-        save_hash_location(mer_str, i)
-
-def create_reference_hash_memory(start_idx, stop_idx, mer_length, reference):
-
-    # when stop idx is too large
-    if stop_idx + mer_length >= len(reference):
-        stop_idx = len(reference) - mer_length
-
-    all_hash_locaton = {}
-
-    for i in xrange(start_idx, stop_idx):
-        if i % 10000 == 0:
-            print "finished %d/%d, percent=%.2f" % (i, stop_idx - start_idx, (i * 100 / (stop_idx-start_idx)))
-        mer = reference[i:i+mer_length]
-        if mer in all_hash_locaton:
-            all_hash_locaton[mer].append(i)
-        else:
-            all_hash_locaton[mer] = [i]
-
-    with open("all_hash_location.pickle", "wb") as f:
-        pickle.dump(all_hash_locaton, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-def work_small_job(datafile, start_idx, stop_idx):
-    global all_ref
-    if datafile not in all_ref:
-        all_ref[datafile] = commonlib.read_reference_genome('dataset/%s/ref.txt' % datafile)
-    create_reference_hash(start_idx,stop_idx,mer_length, all_ref[datafile])
+        
+        # push to redis in a list with the key as mer length
+        r.rpush(mer, i)
 
 if __name__ == "__main__":
-
-    if 0:
-        # delete all saved reference genome hash
-        db = database.create_database_connection()
-        db.execute("DELETE FROM reference_hash")
-
-        # read reference genome and populate database
-        reference_arr = commonlib.read_reference_genome('dataset/practice2/ref.txt')
-        create_reference_hash(0,len(reference_arr),mer_length, reference_arr)
-
-    else:
-        # read reference genome and populate database
-        # reference_arr = commonlib.read_reference_genome_bare('dataset/practice2/ref.txt')
-        reference_arr = commonlib.read_reference_genome_bare('dataset/ugrad/ref.txt')
-        
-        # create_reference_hash_memory(0,len(reference_arr),mer_length, reference_arr)
+    redis_db=["10k", "1m", "100m"].index(dataset)
+    reference_arr = commonlib.read_reference_genome_bare('dataset/%s/ref.txt' % dataset)
+    create_reference_hash_save_redis(mer_length, reference_arr, redis_path, redis_db=redis_db)
 
