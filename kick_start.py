f46984bbe3db08805ddde92cd4e3ba2b5a5fe5d4
mqchau <quanqchau@gmail.com>
2016-04-21 13:16:58 -0700

    remove old unnecessary files

--- kick_start.py
+++
@@ -1,7 +0,0 @@
-import sqs_client
-
-if __name__ == "__main__":
-    # sqs_client.send_message('start_job', 'align_base,practice2')
-    # sqs_client.send_message('start_job', 'align_base,ugrad')
-    # sqs_client.send_message('start_job', 'get_mutation,ugrad')
-    sqs_client.send_message('start_job', 'get_mutation,practice2')
